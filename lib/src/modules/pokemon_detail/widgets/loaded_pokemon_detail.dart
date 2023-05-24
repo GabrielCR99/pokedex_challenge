7263c7a509119caecb3fb4265581fdc59b4f39f5
Roveri <gabriel.roveri7@gmail.com>
2023-05-23 22:29:44 -0300

    update loaded_pokemon_detail.dart

--- lib/src/modules/pokemon_detail/widgets/loaded_pokemon_detail.dart
+++ lib/src/modules/pokemon_detail/widgets/loaded_pokemon_detail.dart
@@ -206,7 +206,6 @@ class LoadedPokemonDetail extends StatelessWidget {
                           ),
                         ),
                       ),
-                      // PokemonBaseStatsTile(pokemonDetail: pokemonDetail),
                     ],
                   ),
                 ),
